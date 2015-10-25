b182dc3cf1d1dc15a8ff14f9e5be0b7ef8ab5e0a
Julia Biro <julia.biro@prezi.com>
2015-10-25 22:12:34 +0100

    fix attribute parsing

--- chef-plugin.py
+++ chef-plugin.py
@@ -97,63 +97,87 @@ class BuildRecipeTree(FindRecipeCommand):
 	# repeat 3-5 until start list is empty
 
 	_recipes_tree={}
+	_attributes_tree={}
+	_all_json=None
 	
-	
-
 	def run(self, edit):
 		if not self._validate_chef_root():
 			return
+
+		import json
+		selection = sublime.Region(0, self.view.size())
+		self._all_json=None
+		try:
+			self._all_json=json.loads(self.view.substr(selection))
+		except Exception as e:
+			print(e, "not a json file")
+			return 
+
+		self.build_recipes_tree()
+		print("recipes_tree: ", self._recipes_tree)
+			
+		self.build_attributes_tree()
+		print("attributes_tree: ", self._attributes_tree)
+
+
+	def find_attribute_use(self):
+		#1. get attribute name
+		# 2. go up to attribute group, make note of path
+		# 3. find in recipes
+		pass
+
+	def build_recipes_tree(self):
+		if self._recipes_tree!={} or not self._all_json.has_key("run_list"):
+			return 
 
 		def _get_name(name):
 			s=name.find('[')
 			e=name.find(']', s+1)
 			return name[s+1:e]
 
-		import json
-		selection = sublime.Region(0, self.view.size())
-		all_json=None
-		try:
-			all_json=json.loads(self.view.substr(selection))
-		except Exception as e:
-			print(e, "not a json file")
+		def _copy_recipe_to_tree(RT, recipe_name):
+			if recipe_name.find("::")!=-1:
+				delim=recipe_name.find('::')
+				if RT.has_key(recipe_name[:delim]):
+					RT[recipe_name[:delim]].append(recipe_name[delim+2:])
+				else:
+					RT[recipe_name[:delim]]=[recipe_name[delim+2:]]
+			else:
+				RT[recipe_name]=['default']
+	
+		def _get_role_runlist(RP, role_name):
+			import os
+			import json
+			role_file=os.path.join(RP, role_name+".json")
+
+			#read role file
+			# find runlist
+			try:
+				rolefile=open(role_file, "r")
+				rl=json.loads(rolefile.read())
+				return rl["run_list"]
+			except Exception as e:
+				print(e)
+
 		
-		runlist=all_json["run_list"]
+		runlist=self._all_json["run_list"]
 		while runlist!=[]:
-			print("recipes_tree: ", self._recipes_tree)
 			roles_list=[x for x in runlist if x and x.startswith("role")]
 			recipes_list=[x for x in runlist if x and x.startswith("recipe")]
 			
 			for recipe in recipes_list:
-				self._copy_recipe_to_tree(_get_name(recipe))
+				_copy_recipe_to_tree(self._recipes_tree, _get_name(recipe))
 
 			runlist=[]
 			for role in roles_list:
-				runlist.extend(self._get_role_runlist(_get_name(role)))
+				runlist.extend(_get_role_runlist(self._roles_path, _get_name(role)))
 
 		
-	def _copy_recipe_to_tree(self, recipe_name):
-		if recipe_name.find("::")!=-1:
-			delim=recipe_name.find('::')
-			if self._recipes_tree.has_key(recipe_name[:delim]):
-				self._recipes_tree[recipe_name[:delim]].append(recipe_name[delim+2:])
-			else:
-				self._recipes_tree[recipe_name[:delim]]=[recipe_name[delim+2:]]
-		else:
-			self._recipes_tree[recipe_name]=['default']
-	
-	def _get_role_runlist(self, role_name):
-		import os
-		import json
-		role_file=os.path.join(self._roles_path, role_name+".json")
-
-		#read role file
-		# find runlist
-		try:
-			rolefile=open(role_file, "r")
-			rl=json.loads(rolefile.read())
-			return rl["run_list"]
-		except Exception as e:
-			print(e)
-
-
+	def build_attributes_tree(self):
+		for key in [ "normal", "default_attributes", "override_attributes"]:
+			if self._all_json.has_key(key):
+				if self._attributes_tree.has_key(key):
+					self._attributes_tree[key].update(self._all_json[key])
+				else:
+					self._attributes_tree[key]=(self._all_json[key])
 # TODO: parse json for default attributes, override_attribtes, and for normal tag in nodes
