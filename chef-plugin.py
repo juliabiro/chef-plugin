bbcb5932b9cc126a3ba97fae9a7c185c356a3d23
Julia Biro <julia.biro@prezi.com>
2015-10-25 16:05:54 +0100

    look for chef root automatially. the discovery is run once for every opened file. fixed path issues

--- chef-plugin.py
+++ chef-plugin.py
@@ -1,13 +1,16 @@
 import sublime, sublime_plugin
 
 class FindRecipeCommand(sublime_plugin.TextCommand):
-	_cookbooks_path="/Users/juliabiro/.prezi/prezi-chef/cookbooks/"
-	_roles_path="/Users/juliabiro/.prezi/prezi-chef/roles/"
+	_cookbooks_path=None
+	_roles_path=None
 
 	def run(self, edit):
-		
+		if not self._validate_chef_root():
+			return
+
 		sels=self.view.sel()
 		for s in sels:
+			filename=None
 			if self._is_recipe(s):
 				if self._is_included_recipe(s):
 					filename = self._find_recipe_path(self._get_recipe_name(s, delim1='\"', delim2='\"')) 
@@ -17,7 +20,8 @@ class FindRecipeCommand(sublime_plugin.TextCommand):
 			elif self._is_role(s):
 				filename = self._find_role_path(self._get_recipe_name(s))
 		
-		self.view.window().open_file(filename, sublime.ENCODED_POSITION)
+			if filename:
+				self.view.window().open_file(filename, sublime.ENCODED_POSITION)
 		
 	def _get_recipe_name(self, selection, delim1='\[', delim2='\]'):
 		line = self.view.line(selection)
@@ -27,7 +31,7 @@ class FindRecipeCommand(sublime_plugin.TextCommand):
 
 	def _is_recipe(self, selection):
 		line = self.view.line(selection)
-		return line.contains(self.view.find("recipe", line.begin()))
+		return self.view.find("recipe", line.begin()) and line.contains(self.view.find("recipe", line.begin()))
 
 	def _is_included_recipe(self, selection):
 		line = self.view.line(selection)
@@ -35,7 +39,7 @@ class FindRecipeCommand(sublime_plugin.TextCommand):
 		
 	def _is_role(self, selection):
 		line = self.view.line(selection)
-		return line.contains(self.view.find("role", line.begin()))
+		return self.view.find("role", line.begin()) and line.contains(self.view.find("role", line.begin()))
 		
 	def _find_recipe_path(self, recipe_name):
 		# case 2: recipe name only
@@ -48,6 +52,38 @@ class FindRecipeCommand(sublime_plugin.TextCommand):
 
 	def _find_role_path(self,role_name):
 		return self._roles_path+role_name+".json"
+
+	# make sure that we have a chef root somewhere. 
+	# if already set, check if we are on thhe path
+	# if not, make sure to find one 
+	def _validate_chef_root(self):
+		import os
+		def update_chef_root():
+
+			def find_cookbook_dir():
+				
+				cbp = None
+				current_dir=os.path.dirname(os.path.realpath(self.view.file_name()))
+				
+				i=100
+				while i>0 and not cbp and os.path.dirname(current_dir) != current_dir:
+					for dirs in os.listdir(current_dir):
+						if dirs.endswith("cookbooks"):
+							cbp= os.path.join(current_dir, dirs+"/")
+							break
+					current_dir=os.path.realpath(os.path.join(current_dir, os.pardir))
+					i=i-1
+				return cbp
+					
+			self._cookbooks_path=find_cookbook_dir()
+			
+			if self._cookbooks_path!=None:
+				self._roles_path=os.path.join(self._cookbooks_path, "../roles/")
+
+			return (self._cookbooks_path and self._roles_path)
+
+		return (self._cookbooks_path and self._roles_path) or update_chef_root()
+
 
 """
 recipe "[users]"
