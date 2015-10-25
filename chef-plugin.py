e974681dc1c32e137b1ca26738fba539038382ec
Julia Biro <julia.biro@prezi.com>
2015-10-25 12:49:26 +0100

    can differentiate between roles and recipes. can construct role and recipe paths

--- chef-plugin.py
+++ chef-plugin.py
@@ -2,17 +2,39 @@ import sublime, sublime_plugin
 
 class FindRecipeCommand(sublime_plugin.TextCommand):
 	_cookbooks_path="/Users/juliabiro/.prezi/prezi-chef/cookbooks/"
+	_roles_path="/Users/juliabiro/.prezi/prezi-chef/roles/"
 	
 	def run(self, edit):
-		print(self._get_selected_text())
-		#self.view.insert(edit, 0, self._find_recipe_path())
-
-
-	def _get_selected_text(self):
 		sels=self.view.sel()
 		for s in sels:
-			return(self.view.substr(s))
+			if self._is_recipe(s):
+				print self._find_recipe_path(self._get_full_recipe_name(s))
+			elif self._is_role(s):
+				print self._find_role_path(self._get_full_recipe_name(s))
 
-	def _find_recipe_path(self):
+	def _get_full_recipe_name(self, selection):
+		line = self.view.line(selection)
+		open = self.view.find('\[', line.begin())
+		close = self.view.find('\]', line.begin())
+		return self.view.substr(sublime.Region(open.begin()+1,close.end()-1))
 
-		return str(self._get_selected_text())
+	def _is_recipe(self, selection):
+		line = self.view.line(selection)
+		return line.contains(self.view.find("recipe", line.begin()))
+		
+	def _is_role(self, selection):
+		line = self.view.line(selection)
+		return line.contains(self.view.find("role", line.begin()))
+		
+	def _find_recipe_path(self, recipe_name):
+		# case 2: recipe name only
+		# case 1: cookbook name:: recipe name
+		delim = recipe_name.find("::")
+		if  delim == -1:
+			return self._cookbooks_path+recipe_name+"/recipes/default.rb"
+		else:
+			return self._cookbooks_path+recipe_name[:delim]+"/recipes/"+recipe_name[delim+2:]+".rb"
+
+	def _find_role_path(self,role_name):
+		return self._roles_path+role_name+".json"
+
